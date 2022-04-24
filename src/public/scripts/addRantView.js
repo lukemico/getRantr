7327e7e22fb7adf130cebbe6eb848eb735fb20fc
Luke Mico <uvvhibt6qc@privaterelay.appleid.com>
2022-04-24 18:51:16 +1000

    Check typeof text variable in onChange function

--- src/public/scripts/addRantView.js
+++ src/public/scripts/addRantView.js
@@ -1,32 +1,30 @@
 var AddRantView = Backbone.View.extend({
+	initialize: function () {
+		this.$('textarea').on('keyup', this.onChange.bind(this));
+		this.$('form').on('submit', this.onSubmit.bind(this));
+		var text = localStorage.getItem('next-rant');
+		if (text) {
+			this.$('textarea').val(text);
+		}
+	},
 
-  initialize: function() {
-    this.$('textarea').on('keyup', this.onChange.bind(this));
-    this.$('form').on('submit', this.onSubmit.bind(this));
-    var text = localStorage.getItem('next-rant');
-    if (text) {
-      this.$('textarea').val(text);
-    }
-  },
+	onChange: function (evt) {
+		var text = (evt.target || {}).value;
+		if (typeof text === 'string') {
+			localStorage.setItem('next-rant', text);
+		}
+	},
 
-  onChange: function(evt) {
-    var text = (evt.target || {}).value;
-    if (text) {
-      localStorage.setItem('next-rant', text);
-    }
-  },
+	onSubmit: function (evt) {
+		evt.preventDefault();
+		var form = evt.target;
+		var rant = {
+			text: form.rant.value,
+		};
 
-  onSubmit: function(evt) {
-    evt.preventDefault();
-    var form = evt.target;
-    var rant = {
-      text: form.rant.value
-    };
+		analytics.trackConversion();
+		this.collection.create(rant, { wait: true });
 
-    analytics.trackConversion();
-    this.collection.create(rant, { wait: true });
-
-    form.rant.value = '';
-  }
-
+		form.rant.value = '';
+	},
 });
